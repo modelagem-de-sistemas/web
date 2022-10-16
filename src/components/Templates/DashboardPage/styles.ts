2e55e6f5e0a0529a5ecf3b1d925eeff5e78b7e92
Charles Lelis Braga <charles.braga.jf.cb@gmail.com>
2022-10-15 22:23:39 -0300

    feature: finish admin

--- src/components/Templates/DashboardPage/styles.ts
+++ src/components/Templates/DashboardPage/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  > main {
+    max-width: 1600px;
+    padding: calc(var(--header-height) + 2rem) 3rem 3rem 6rem;
+
+    @media screen and (max-width: 768px) {
+      padding: calc(var(--header-height) + 1rem) 1rem 0 1rem;
+    }
+  }
+`;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 import DashboardPage from '@/components/Templates/DashboardPage';
 
 const DashboardHome: NextPage = () => {
-  return <DashboardPage></DashboardPage>;
+  return (
+    <DashboardPage>
+      <h1>Dashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cum modi praesentium quasi at delectus ullam repellat, doloribus
+        laboriosam incidunt magni suscipit molestias consequuntur distinctio voluptatibus ea ducimus explicabo porro.
+      </p>
+    </DashboardPage>
+  );
 };
 
 export default DashboardHome;
